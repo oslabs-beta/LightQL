acd9602d91bdd23da57c75193cb83090be7754cf
cyrusyari <cyrus.brk@gmail.com>
2022-11-10 16:43:46 +0000

    database creation and testing updates to main cache file

---
+++ npm-package/Demo/events.sql
@@ -0,0 +1,13 @@
+create table simple_events (
+  id text,
+  name text,
+ start_date text,
+ primary_venue_id text,
+ summary text,
+ image text,
+ value integer,
+ primary_venue text,
+ address text,
+ city text,
+  ticket_available boolean
+);
