c36d1256acfb9c6dbed187da8bbec63a0259b530
Cassidy Johnson <cassidyrose56@gmail.com>
2022-11-26 14:16:36 -0600

    lots of updates on about us page and fixed chart size

--- src/client/webpage/components/AboutUs.js
+++ src/client/webpage/components/AboutUs.js
@@ -2,21 +2,55 @@ import React from 'react';
 import SingleTile from './aboutUsComponents/SingleTile';
 import '../styling/aboutUs.scss';
 
+const exPic = require('../../../assets/prof-pic-placeholder.jpeg');
+const drew = require('../../../assets/drew.png');
+
+const cyrus = require('../../../assets/cyrus.png');
+
 const AboutUs = () => {
 
     return (
-        <div id='aboutUsLayout'>
-            <section id='topOfPage'>
-                <SingleTile key='Cassidy' headshot='ex' />
-                <SingleTile key='Drew' headshot='ex' />
-                <SingleTile key='Pierce' headshot='ex' /> 
-            </section>
-            <section id='bottomOfPage'>
-                <SingleTile key='Cyrus' headshot='ex' /> 
-                <SingleTile key='Rhea' headshot='ex' /> 
-            </section>
-            
+        <div>
+            <h1 id="aboutUsTitle">Meet the Team</h1>
+            <section id='aboutUsLayout'>
+                    <SingleTile 
+                        key='Cassidy' 
+                        name='Cassidy Johnson' 
+                        githubLink='https://github.com/cassidyrose56'
+                        linkedInLink='https://www.linkedin.com/in/cassidy-r-johnson/'
+                        headshot={exPic} 
+                    />
+                    <SingleTile 
+                        key='Drew' 
+                        name='Drew Tucker' 
+                        githubLink='https://github.com/Drew-tucker33'
+                        linkedInLink='https://www.linkedin.com/in/drew-t-4369199b/'
+                        headshot={drew} 
+                    />
+                    <SingleTile 
+                        key='Pierce' 
+                        name='Pierce Heska' 
+                        githubLink='https://github.com/pheska'
+                        linkedInLink='https://www.linkedin.com/in/pheska/'
+                        headshot={exPic} 
+                    /> 
+                    <SingleTile 
+                        key='Cyrus' 
+                        name='Cyrus Yari' 
+                        githubLink='https://github.com/cyrusyari'
+                        linkedInLink='https://www.linkedin.com/in/cyrusyari/'
+                        headshot={cyrus} 
+                    /> 
+                    <SingleTile 
+                        key='Rhea' 
+                        name='Rhea Wu' 
+                        githubLink='https://github.com/rheawu1212'
+                        linkedInLink='https://www.linkedin.com/in/rheawu-tech/'
+                        headshot={exPic} 
+                    /> 
+            </section> 
         </div>
+       
         
     )
     
--- src/client/webpage/components/aboutUsComponents/SingleTile.js
+++ src/client/webpage/components/aboutUsComponents/SingleTile.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import '../../styling/aboutUs.scss';
+import { motion } from 'framer-motion';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
-const SingleTile = () => {
+
+const SingleTile = ({key, name, headshot, githubLink, linkedInLink}) => {
 
     return (
         <div id='singleTile'>
+            <img id='headshots' src={headshot.default} />
+            <section id='bottom-of-tile'>
+                <h2 id='name'>{name}</h2>
+                <section id='contact-links'>
+                <a 
+					href={githubLink}
+					target="_blank">
+					<GitHubIcon className='au-icons' sx={{color: '#323949'}}></GitHubIcon>
+				</a>
+                <a 
+					href={linkedInLink}
+					target="_blank">
+					<LinkedInIcon className='au-icons' sx={{color: '#323949'}}></LinkedInIcon>
+				</a>
+                </section>
+            </section>
 
         </div>
     )
